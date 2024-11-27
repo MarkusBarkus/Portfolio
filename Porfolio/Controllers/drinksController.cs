95282c0b173075568bd5aad319d9613a59724e04
Mohmd Alsaidi <m_alsaidi@fanshaweonline.ca>
2024-11-27 14:37:11 -0500

    Added Html and js documents and one style sheet for portfolio website, have two projects that will be used currently. Will add more later.

---
+++ Porfolio/Controllers/drinksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Porfolio.Controllers
+{
+    public class drinksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
